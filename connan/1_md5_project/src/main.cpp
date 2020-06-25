dbc25b7449587bb1440c18985adf6b5f7df367f7
Pavel Ershov <pershov@game-insight.com>
2020-06-25 14:02:05 +0300

    Some improvements in platform profiles

--- connan/1_md5_project/src/main.cpp
+++ connan/1_md5_project/src/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
-#include <Poco/MD5Engine.h>
-#include <Poco/DigestStream.h>
+
+// TODO: Абсолютные или относительные пути?
+#include <curl/curl.h>
+#include <zlib.h>
+#include <assimp/anim.h>
+#include <openssl/opensslv.h>
+//#include <Poco/MD5Engine.h>
+//#include <Poco/DigestStream.h>
 
 
 int main(int argc, char** argv) {
-    Poco::MD5Engine md5;
+    /*Poco::MD5Engine md5;
 
     Poco::DigestOutputStream ds(md5);
     ds << "abcdefghijklmnopqrstuvwxyz";
     ds.close();
 
-    std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
+    std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;*/
     
     return 0;
 }
